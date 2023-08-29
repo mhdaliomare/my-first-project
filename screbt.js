9c368d5203f0ffec3b5259aa3d84075e1e63c574
mhammedali <132587041+mhammedali@users.noreply.github.com>
2023-08-28 17:12:55 -0700

    first commit

---
+++ screbt.js
@@ -0,0 +1,21 @@
+let mystreng ="xxxtintasum";
+console.log(mystreng);
+
+let mynumber=6;
+console.log(mynumber);
+
+let myboolean=true;
+console.log(myboolean);
+
+let myundefined
+console.log(myundefined);
+
+let myobject={
+name:"ali",
+age:"20",
+
+}
+console.log(myobject);
+ 
+let myarray= [ "ali","ahmed","mohemmed" ];
+console.log(myarray);
